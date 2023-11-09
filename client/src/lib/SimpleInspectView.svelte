5b9c41461aaf4f7f47a058ef255bf81b625ef56f
Nahuel Rabey <nahuelrabeywork@gmail.com>
2023-11-09 18:10:59 -0300

    Add SimpleInspectView component with EmaMomentumView subcomponents.

---
+++ client/src/lib/SimpleInspectView.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import EmaMomentumView from "./EMAMomentumView.svelte";
+    export let ticker: string;
+</script>
+
+<div>
+  <EmaMomentumView interval={"1m"} ticker={ticker}/>
+  <EmaMomentumView interval={"1h"} ticker={ticker}/>
+  <EmaMomentumView interval={"1d"} ticker={ticker}/>
+  <EmaMomentumView interval={"1w"} ticker={ticker}/>
+</div>
