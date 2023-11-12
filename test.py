c2e205b5a2cb676fd81eff6fd8efede6e9e36d62
Nahuel Rabey <nahuelrabeywork@gmail.com>
2023-11-12 16:34:11 -0300

    Add test.py and update .gitignore

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
-ge_GNO = 14.5/13.9
-ge_ADA = 14.5/13.97
-ge_MATIC = 14.5/14.04
-ge_LINK = 14.5/13.96
+# ge_GNO = 14.5/13.9
+# ge_ADA = 14.5/13.97
+# ge_MATIC = 14.5/14.04
+# ge_LINK = 14.5/13.96
 
-pc_GNO = 163.3
-pc_ADA = 0.3795
-pc_MATIC = 0.86
-pc_LINK = 14.55
+# pc_GNO = 163.3
+# pc_ADA = 0.3795
+# pc_MATIC = 0.86
+# pc_LINK = 14.55
 
-print(pc_GNO*ge_GNO)
-print(pc_ADA*ge_ADA)
-print(pc_MATIC*ge_MATIC)
-print(pc_LINK*ge_LINK)
+# print(pc_GNO*ge_GNO)
+# print(pc_ADA*ge_ADA)
+# print(pc_MATIC*ge_MATIC)
+# print(pc_LINK*ge_LINK)
+
+ge = 20.5/20
+pc = 15.64
+print(ge*pc)
